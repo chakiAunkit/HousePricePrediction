4f3bf7245ce9a96263e52d703c5cca1cbbf72c17
unknown <agnidipto12.25@gmail.com>
2022-06-05 02:35:15 +0530

    Logging enabled

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+import sys
+import logging
 import util
 
 app = Flask(__name__)
 # cors = CORS(app, resources={r"/*": {"origins": "*"}})
 # app.config['CORS_HEADERS'] = 'Content-Type'
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 CORS(app, support_credentials=True)
 
