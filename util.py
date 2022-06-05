0e588401fcfd7a6b2322554496f27a0e7c985396
unknown <agnidipto12.25@gmail.com>
2022-06-05 17:03:26 +0530

    modified util

--- util.py
+++ util.py
@@ -26,7 +26,7 @@ def get_estimated_price(location, sqft, bhk, bath):
 
 def get_location_names():
     global __locations
-    return __locations
+    return datadict
 
 
 def load_saved_artifacts():
